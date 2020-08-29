faf43254409e23121b70cc0f7410bcdb5d6069c5
Pierre-Nicolas Perrin <pnp@jgp.net>
2020-08-29 01:50:15 -0400

    add time functionality to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,45 @@
 import React from "react"
 
 function Header() {
+    const firstName = "Pierre-Nicolas"
+    const lastName = "Perrin"
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    const styles = {
+        color: "#FF8C00",
+        fontSize: 50
+    }
+
+    const stylesTime = {
+        fontSize: 30
+    }
+
+
+    if(hours < 12) {
+        timeOfDay="morning"
+        stylesTime.color = "#04756F"
+    }
+    else if(hours >= 12 && hours <18) {
+        timeOfDay="afternoon"
+        stylesTime.color = "#8914A3"
+    }
+    else {
+        timeOfDay = "night"
+        styles.color = "#d90000"
+    }
+
     return (
-        <header className="navbar">
-            <h1>Pierre-Nicolas' Website</h1>
-        </header>
-)
+        <div>
+            <header className="navbar">
+                <h1 style={styles}>{`${firstName} ${lastName}`}</h1>
+            </header>
+            <p style={stylesTime}>It is currently {hours} o'clock.
+            Good {timeOfDay}!
+            </p>
+        </div>
+    )
 }
 
 export default Header
