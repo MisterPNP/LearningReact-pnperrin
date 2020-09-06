e48b58d6a06243886de2672901d65bb0de16e016
Pierre-Nicolas Perrin <pnp@jgp.net>
2020-09-06 11:57:23 -0400

    learn event handling

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -11,12 +11,21 @@ import React from "react"
 //passing in props passes in the names input in MyApp
 //they become properties passed in for customization
 function ContactCard(props) {
+
+    function userMouseOver() {
+        console.log("Mouse is Over Button. Click!")
+    }
     return (
         <div className="contactCard">
             <h3>{props.contact.name}</h3>
             <img src={props.contact.imgURL} alt="cat"/>
             <p> Phone: {props.contact.phone}</p>
             <p> Email: {props.contact.email}</p>
+
+            <button onClick={function() {console.log("Clicked")}}
+                    onMouseOver={userMouseOver}>
+                Adopt me!
+            </button>
         </div>
     )
 }
